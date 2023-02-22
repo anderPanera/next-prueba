9858cd7025257c0d687c3942800e8f56494909ec
anderPanera <anderpanera@gmail.com>
2023-02-22 13:57:24 +0100

    cambio contenido

--- app/page.js
+++ app/page.js
@@ -1,45 +1,7 @@
 export default function App() {
     return (
         <>
-            <div class="lateral martop marbot"></div>
-            <div role="main" id="main" class="main martop">
-                <h1 class="black">ZAPAS</h1>
-                <p class="black">Esto es una celebración de todo lo que nos mueve. Del poder de elegir y de la libertad de ser. Rendimos homenaje al hermoso caos del movimiento y a la plena consciencia que tan bien nos hacen sentir.</p>
-            </div>
-            <div class="uno textr">
-                <h2 class="black">AirMax 720</h2>
-                <p><a href="pages/productos.html" class="black">Compralas</a></p>
-            </div>
-            <div class="unoimg"><img src="media/zapas1.webp" alt="zapatillas de ejemplo 1"/></div>
-            <div class="main">
-                <h2 class="black">ZAPAS</h2>
-                <p class="black">Tienda internacional de zapatillas con envíos gratuitos. Fácil de comprar, obtén tu pedido antes de 3 días haciendo el pedido ahora.</p>
-            </div><div class="helper"></div>
-            <div class="unoimg"><img src="media/zapas2.webp" alt="zapatillas de ejemplo 2"/></div>
-            <div class="uno textl">
-                <h2 class="black">AirMax 720</h2>
-                <p><a href="pages/productos.html" class="black">Compralas</a></p>
-            </div>
-            <div class="main marbot">
-                <h2 class="black">ZAPAS</h2>
-                <p class="black">Tenemos todas las zapatillas del mercado, elige las que te gusten y cómpralas, se permiten devoluciones.</p>
-            </div>
-            <div class="main marbot">
-                <h2 class="black">ZAPAS</h2>
-                <p class="black">Tenemos todas las zapatillas del mercado, elige las que te gusten y cómpralas, se permiten devoluciones.</p>
-            </div>
-            <div class="main marbot">
-                <h2 class="black">ZAPAS</h2>
-                <p class="black">Tenemos todas las zapatillas del mercado, elige las que te gusten y cómpralas, se permiten devoluciones.</p>
-            </div>
-            <div class="main marbot">
-                <h2 class="black">ZAPAS</h2>
-                <p class="black">Tenemos todas las zapatillas del mercado, elige las que te gusten y cómpralas, se permiten devoluciones.</p>
-            </div>
-            <div class="main marbot">
-                <h2 class="black">ZAPAS</h2>
-                <p class="black">Tenemos todas las zapatillas del mercado, elige las que te gusten y cómpralas, se permiten devoluciones.</p>
-            </div>
+            
         </>
     )
 }
